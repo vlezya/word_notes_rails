67bab2263081c05def580acdacdb4372d8ebd6e0
Stanislav Vlezko <vlezyashop@gmail.com>
2021-10-12 18:52:12 +0300

    add active_model_serializers

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,41 +1,46 @@
 class Api::V1::CardsController < ApplicationController
   before_action :set_card, only: [:show, :update, :destroy]
 
-  # GET /cards
+  # GET /api/v1/cards
   def index
     @cards = Card.all
-
-    render json: @cards
+    cards_json = ActiveModel::Serializer::CollectionSerializer.new(@cards, each_serializer: CardSerializer)
+    render json: { cards: cards_json }
   end
 
-  # GET /cards/1
+  # GET /api/v1/cards/1
   def show
-    render json: @card
+    card_json = CardSerializer.new(@card).as_json
+    render json: { card: card_json }
   end
 
-  # POST /cards
+  # POST /api/v1/cards
   def create
     @card = Card.new(card_params)
 
     if @card.save
-      render json: @card, status: :created, location: @card
+      card_json = CardSerializer.new(@card).as_json
+      render json: { card: card_json }, status: :created
     else
       render json: @card.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /cards/1
+  # PATCH/PUT  /api/v1/cards/1
   def update
     if @card.update(card_params)
-      render json: @card
+      card_json = CardSerializer.new(@card).as_json
+      render json: { card: card_json }
     else
       render json: @card.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /cards/1
+  # DELETE /api/v1/cards/1
   def destroy
     @card.destroy
+    card_json = CardSerializer.new(@card).as_json
+    render json: { card: card_json }
   end
 
   private
