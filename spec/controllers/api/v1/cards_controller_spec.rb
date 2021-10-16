159ad747f4e2f651d1a050165794c66c053b5103
Stanislav Vlezko <vlezyashop@gmail.com>
2021-10-16 20:28:24 +0300

    tested cards_controller/action :show

--- spec/controllers/api/v1/cards_controller_spec.rb
+++ spec/controllers/api/v1/cards_controller_spec.rb
@@ -1,10 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::CardsController, type: :controller do
+  
   describe 'routing' do
     it 'routes GET /api/v1/cards to api/v1/cards#index' do
-      expect(get: '/api/v1/cards').to route_to(controller: 'api/v1/cards',
-         action: 'index', format: 'json')
+      expect(get: '/api/v1/cards').to route_to(
+        controller: 'api/v1/cards',
+        action: 'index',
+        format: 'json'
+      )
+    end
+    
+    it 'routes GET /api/v1/cards/:id to api/v1/cards#show' do
+      expect(get: '/api/v1/cards/55').to route_to(
+        controller: 'api/v1/cards',
+        action: 'show',
+        id: '55',
+        format: 'json'
+      )
     end
   end
   
@@ -17,7 +30,6 @@ RSpec.describe Api::V1::CardsController, type: :controller do
     before :each do
       get :index
     end
-  
     
     it 'is expected to have :ok (200) HTTP response code' do
       expect(response.status).to eq(200)
@@ -26,7 +38,7 @@ RSpec.describe Api::V1::CardsController, type: :controller do
     it 'is expected to have proper amount of Cards' do
       json_response = JSON.parse(response.body)
       expect(json_response.key?('cards')).to eq(true)
-
+      
       cards = json_response['cards']
       expect(cards.count).to eq(5)
     end
@@ -47,6 +59,57 @@ RSpec.describe Api::V1::CardsController, type: :controller do
         expect(card.key?('created_at')).to eq(false)
         expect(card.key?('updated_at')).to eq(false)
       end
+    end
+  end
+  
+  describe 'GET #show' do
+    context 'with valid params' do
+      # 1. Create a global var with Card via FactoryBot
+      before :all do
+        Card.destroy_all
+        @card = FactoryBot.create(:card)
+      end
+      
+      # 2. Add before each with "show" request.
+      before :each do
+        get :show, params: { id: @card.id }
+      end
+      
+      # 3. Write tests for "response code", "return of correct Card", "presense of fields", "absense of fields".
+      it 'is expected to have :ok (200) HTTP response code' do
+        expect(response.status).to eq(200)
+      end
+      
+      it 'is expected to return correct Card' do
+        json_response = JSON.parse(response.body)
+        puts "*"*80
+        puts json_response
+        expect(json_response.key?('card')).to eq(true)
+        
+        card = json_response['card']
+        expect(card).not_to eq(nil)
+        
+        card_id = card['id']
+        expect(card_id).to eq(@card.id)
+      end
+      
+      it 'is expected to include fields' do
+        card = JSON.parse(response.body)['card']
+        expect(card.key?('id')).to eq(true)
+        expect(card.key?('word')).to eq(true)
+        expect(card.key?('translation')).to eq(true)
+        expect(card.key?('example')).to eq(true)
+      end
+      
+      
+      it 'is expected to NOT include fields' do
+        card = JSON.parse(response.body)['card']
+        expect(card.key?('created_at')).to eq(false)
+        expect(card.key?('updated_at')).to eq(false)
+      end
+      
+      # 4. Move existing tests into "with valid params" context.
+      # 5. Write all the tests for "ActiveRecord::RecordNotFound" in context "not found"
     end
   end
 end
