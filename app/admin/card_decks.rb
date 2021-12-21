2642200438d5f07e2d22e19955c26e9b760ac2ba
vlezya <vlezyashop@gmail.com>
2021-12-21 18:12:15 +0200

    update admin active panel

--- app/admin/card_decks.rb
+++ app/admin/card_decks.rb
@@ -1,18 +1,3 @@
 ActiveAdmin.register CardDeck do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :card_id, :deck_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:card_id, :deck_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params :card_id, :deck_id
 end
--- app/admin/decks.rb
+++ app/admin/decks.rb
@@ -1,18 +1,3 @@
 ActiveAdmin.register Deck do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :title, :user_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:title, :user_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params :title, :user_id
 end
